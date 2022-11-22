dfb6e531fe7ae6ce05ef979e2e68429d4ae9cc68
powcoder <powcoder@163.com>
2022-11-22 12:28:28 +0800

    COSI 131a pa3

---
+++ src/main/java/cs131/pa3/CarsTunnels/ConcreteFactory.java
@@ -0,0 +1,33 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+package cs131.pa3.CarsTunnels;
+
+import cs131.pa3.Abstract.Direction;
+import cs131.pa3.Abstract.Factory;
+import cs131.pa3.Abstract.Tunnel;
+import cs131.pa3.Abstract.Vehicle;
+
+/**
+ * The class implementing the Factory interface for creating instances of classes
+ * @author cs131a
+ *
+ */
+public class ConcreteFactory implements Factory {
+
+    @Override
+    public Tunnel createNewBasicTunnel(String name){
+    		throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Vehicle createNewCar(String name, Direction direction){
+    		throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Vehicle createNewSled(String name, Direction direction){
+    		throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
